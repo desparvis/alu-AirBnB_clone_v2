7a8f92347b0669dfd5c10abb2ea0437212d74e2f
desparvis <latifahm501@gmail.com>
2025-02-08 23:12:44 +0200

    web static changes and fixes

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,50 +1,56 @@
 #!/usr/bin/python3
-"""Fabric script that distributes an archive to your web servers"""
-import warnings
-from cryptography.utils import CryptographyDeprecationWarning
-from fabric import Connection
-from os.path import exists
-
-warnings.filterwarnings(action='ignore', category=CryptographyDeprecationWarning)
-
-hosts = ["54.144.93.26", "3.80.93.124"]
-user = "ubuntu"
-key_filename = "~/.ssh/id_rsa"
-
+"""Compress web static package
 """
-function to deploy the archive to the web servers
-"""
+from fabric.api import *
+from datetime import datetime
+from os import path
+
+
+env.hosts = ['3.91.100.7', '3.94.146.13']
+env.user = 'ubuntu'
+env.key_filename = '~/.ssh/id_rsa'
+
+
 def do_deploy(archive_path):
-    """
-    Deploys the archive to web servers.
+        """Deploy web files to server
+        """
+        try:
+                if not (path.exists(archive_path)):
+                        return False
 
-    Args:
-        archive_path (str): Path to the archive file.
+                # upload archive
+                put(archive_path, '/tmp/')
 
-    Returns:
-        bool: True if successful, False otherwise.
-    """
-    if not exists(archive_path):
-        return False
-    try:
-        timestamp = archive_path[-18:-4]
-        path_name = f"/data/web_static/releases/web_static_{timestamp}"
-        
-        for host in hosts:
-            conn = Connection(host=host, user=user, connect_kwargs={"key_filename": key_filename})
-            conn.put(archive_path, "/tmp/")
-            conn.run(f"mkdir -p {path_name}/")
-            conn.run(f"tar -xzf /tmp/web_static_{timestamp}.tgz -C {path_name}/")
-            conn.run(f"rm /tmp/web_static_{timestamp}.tgz")
-            conn.run(f"mv {path_name}/web_static/* {path_name}/")
-            conn.run(f"rm -rf {path_name}/web_static")
-            conn.run('rm -rf /data/web_static/current')
-            conn.run(f"ln -s {path_name}/ /data/web_static/current")
-        
+                # create target dir
+                timestamp = archive_path[-18:-4]
+                run('sudo mkdir -p /data/web_static/\
+releases/web_static_{}/'.format(timestamp))
+
+                # uncompress archive and delete .tgz
+                run('sudo tar -xzf /tmp/web_static_{}.tgz -C \
+/data/web_static/releases/web_static_{}/'
+                    .format(timestamp, timestamp))
+
+                # remove archive
+                run('sudo rm /tmp/web_static_{}.tgz'.format(timestamp))
+
+                # move contents into host web_static
+                run('sudo mv /data/web_static/releases/web_static_{}/web_static/* \
+/data/web_static/releases/web_static_{}/'.format(timestamp, timestamp))
+
+                # remove extraneous web_static dir
+                run('sudo rm -rf /data/web_static/releases/\
+web_static_{}/web_static'
+                    .format(timestamp))
+
+                # delete pre-existing sym link
+                run('sudo rm -rf /data/web_static/current')
+
+                # re-establish symbolic link
+                run('sudo ln -s /data/web_static/releases/\
+web_static_{}/ /data/web_static/current'.format(timestamp))
+        except:
+                return False
+
+        # return True on success
         return True
-    except Exception as e:
-        print(f"Deployment failed: {e}")
-        return False
-
-# Run the script like this:
-# $ python 2-do_deploy_web_static.py versions/file_name.tgz
