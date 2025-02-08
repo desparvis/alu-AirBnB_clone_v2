00766bada7c3e49288449482bcd1c7d350c488ef
desparvis <latifahm501@gmail.com>
2025-02-08 22:01:20 +0200

    deploy web static

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 """Fabric script that distributes an archive to your web servers"""
-from fabric.api import env, put, run
+import warnings
+from cryptography.utils import CryptographyDeprecationWarning
+from fabric import Connection
 from os.path import exists
 
-env.hosts = ["54.144.93.26", "3.80.93.124"]
-env.user = "ubuntu"
-env.key = "~/.ssh/id_rsa"
+warnings.filterwarnings(action='ignore', category=CryptographyDeprecationWarning)
 
+hosts = ["54.144.93.26", "3.80.93.124"]
+user = "ubuntu"
+key_filename = "~/.ssh/id_rsa"
 
 def do_deploy(archive_path):
     """Function to distribute an archive to your web servers"""
@@ -16,18 +19,22 @@ def do_deploy(archive_path):
         file_name = archive_path.split("/")[-1]
         name = file_name.split(".")[0]
         path_name = "/data/web_static/releases/" + name
-        put(archive_path, "/tmp/")
-        run("mkdir -p {}/".format(path_name))
-        run('tar -xzf /tmp/{} -C {}/'.format(file_name, path_name))
-        run("rm /tmp/{}".format(file_name))
-        run("mv {}/web_static/* {}".format(path_name, path_name))
-        run("rm -rf {}/web_static".format(path_name))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {}/ /data/web_static/current'.format(path_name))
+        
+        for host in hosts:
+            conn = Connection(host=host, user=user, connect_kwargs={"key_filename": key_filename})
+            conn.put(archive_path, "/tmp/")
+            conn.run("mkdir -p {}/".format(path_name))
+            conn.run('tar -xzf /tmp/{} -C {}/'.format(file_name, path_name))
+            conn.run("rm /tmp/{}".format(file_name))
+            conn.run("mv {}/web_static/* {}".format(path_name, path_name))
+            conn.run("rm -rf {}/web_static".format(path_name))
+            conn.run('rm -rf /data/web_static/current')
+            conn.run('ln -s {}/ /data/web_static/current'.format(path_name))
+        
         return True
-    except Exception:
+    except Exception as e:
+        print(f"Deployment failed: {e}")
         return False
 
 # Run the script like this:
-# $ fab -f 2-do_deploy_web_static.py
-# do_deploy:archive_path=versions/file_name.tgz
+# $ python 2-do_deploy_web_static.py versions/file_name.tgz
