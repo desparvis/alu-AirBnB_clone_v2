f3cf65039ce47742e6a809eb7f9389da5ed7135a
desparvis <latifahm501@gmail.com>
2025-02-08 17:02:52 +0200

    changes in bash file

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,18 +1,22 @@
 #!/usr/bin/env bash
-# Script that sets up your web servers for the deployment of web_static
+# This sets up the servers for deployment
 
-
+# install nginx, add automatice yes (-y) 
 sudo apt-get -y update
 sudo apt-get -y install nginx
 sudo service nginx start
 
-sudo mkdir -p /data/web_static/shared/
+# create neccessary folders
 sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
 echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html > /dev/null
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
+# set permissions
 sudo chown -R ubuntu:ubuntu /data/
 
+# configure nginx
 sudo sed -i '44i \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
 
+# restart web server
 sudo service nginx restart
